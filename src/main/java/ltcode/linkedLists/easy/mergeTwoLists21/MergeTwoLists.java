343a4c42f9c0d29c685f133734f36e9c19d7da6d
yuqi.li <243583762@qq.com>
2021-09-15 23:17:42 +0800

    linkedlist

---
+++ src/main/java/ltcode/linkedLists/easy/mergeTwoLists21/MergeTwoLists.java
@@ -0,0 +1,42 @@
+package ltcode.linkedLists.easy.mergeTwoLists21;
+
+import ltcode.ds.ListNode;
+
+public class MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+
+    public ListNode mergeTwoListsLoop(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1), p = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if (l1 != null) {
+            p.next = l1;
+        }
+        if (l2 != null) {
+            p.next = l2;
+        }
+        return dummy.next;
+    }
+}
